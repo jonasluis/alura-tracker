3e8939566e4d5633b1b21b19beb074ede1c23528
jonasluis <jonasluis66@gmail.com>
2025-06-20 11:06:52 -0300

    feat: iniciar e pausar o cronomero

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,16 +7,16 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{tempoDecorrido}}</strong>
                     </section>
 
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -33,6 +33,30 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'FormularioTarefa'
+    name: 'FormularioTarefa',
+    data(){
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        tempoDecorrido (): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods: {
+        iniciar() {
+
+            this.cronometro = setInterval(()=>{
+                this.tempoEmSegundos++
+            }, 1000)
+        },
+        
+        finalizar() {
+            clearInterval(this.cronometro)
+        }
+
+    }
 })
 </script>
