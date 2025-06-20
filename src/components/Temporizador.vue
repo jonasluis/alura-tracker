0c94f46f0ee8a32a96fc52440e87195c4197075a
jonasluis <jonasluis66@gmail.com>
2025-06-20 11:56:33 -0300

    refactor: separar o Botao do temporizador

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,24 +1,15 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
-    </div>
-</template>
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
+      <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+      <Botao @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+      <Botao @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
+    </section>
+  </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue';
 
 export default defineComponent({
     name: 'TemporizadorDeTarefas',
@@ -46,6 +37,6 @@ export default defineComponent({
             this.tempoEmSegundos = 0
         }
     },
-    components: {Cronometro}
+    components: {Cronometro, Botao}
 })
 </script>
