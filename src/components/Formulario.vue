620c0b8c6a2cde85591b3da36923434532e86a7c
jonasluis <jonasluis66@gmail.com>
2025-06-20 11:16:22 -0300

    refactor: separar o cronometro do formulário

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -6,9 +6,7 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>{{tempoDecorrido}}</strong>
-                    </section>
+                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
 
                     <button class="button" @click="iniciar">
                         <span class="icon">
@@ -31,6 +29,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
 
 export default defineComponent({
     name: 'FormularioTarefa',
@@ -40,11 +39,7 @@ export default defineComponent({
             cronometro: 0
         }
     },
-    computed: {
-        tempoDecorrido (): string {
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-        }
-    },
+    
     methods: {
         iniciar() {
 
@@ -56,7 +51,7 @@ export default defineComponent({
         finalizar() {
             clearInterval(this.cronometro)
         }
-
-    }
+    },
+    components: {Cronometro}
 })
 </script>
